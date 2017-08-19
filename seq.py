7ee844e79582bb28c70d982a2a7ef9282b300ab5
Dracogenius17 <maximilian-f-98@web.de>
2017-08-19 19:21:07 +0200

    seq changes

--- seq.py
+++ seq.py
@@ -9,11 +9,9 @@ import numpy as np
 import pandas as pd
 from static_property import static_property
 import types
-<<<<<<< HEAD
 import scipy.signal as sps
 import matplotlib.pylab as plt
-=======
->>>>>>> b3292f1dc4b18d2f7f956fd13e9d0332a640a2a1
+
 
 class seq(str):
     """A sequence"""  
@@ -58,15 +56,13 @@ class seq(str):
         elif isinstance(fun_or_dic, types.DictionaryType):
             return [fun_or_dic[char] for char in self]
             
-<<<<<<< HEAD
     #map plotting(moving average), recurrence/fourier analysis
     def plot_map(self, fun_or_dic, window=1):
         array = np.array(self.map(fun_or_dic))
         conv = sps.convolve(array, 1.0/window**np.ones((window)))
         plt.plot(conv)
         plt.show()
-=======
->>>>>>> b3292f1dc4b18d2f7f956fd13e9d0332a640a2a1
+
 
 class DNA(seq):
     """A DNA sequence"""
@@ -81,36 +77,15 @@ class DNA(seq):
     def gc(self):
         return (self.composition['G']+self.composition['C']) * 100.0/len(self)        
      
-<<<<<<< HEAD
- 
+
 class protein(seq):
     def __init__():
         pass
     #load aaindex
-=======
-     
-from random import randint
-def random_base(RNAflag = False):
-    return ('UCAG' if RNAflag else 'TCAG')[randint(0,3)]
-def random_codon(RNAflag = False):
-    return random_base(RNAflag) + random_base(RNAflag) + random_base(RNAflag)
-
-def read_FASTA(filename):
-    with open(filename) as file:
-        return [(part[0].split('|'), part[2].replace('\n', ''))
-                 for part in [entry.partition('\n')
-                 for entry in file.read().split('>')[1:]]]    
-    
->>>>>>> b3292f1dc4b18d2f7f956fd13e9d0332a640a2a1
-    
     
 if __name__ == '__main__':    
     s = DNA('ACTGATCG')
-<<<<<<< HEAD
     s.plot_map(lambda char: 1 if (char == 'A' or char == 'G') else 0, 2)
-=======
-    print(s.gc)
->>>>>>> b3292f1dc4b18d2f7f956fd13e9d0332a640a2a1
     
     
 #regular expressions
